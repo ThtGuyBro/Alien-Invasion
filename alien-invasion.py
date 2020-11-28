33be61e14b6444e333e6a2e09aba43caf899f79f
[ThtGuyBro} <[mrlltalentandmedia@gmail.com]>
2020-11-28 12:51:20 -0800

    Added ship sprite and made it move

--- alien-invasion.py
+++ alien-invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
 
@@ -10,16 +11,43 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.sceen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
 
     def run_game(self):
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            self.screen.fill(self.settings.bg_color)
-            pygame.display.flip()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+            
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == ord('s'):
+                    self.ship.moving_down = True
+                elif event.key == ord('d'):
+                    self.ship.moving_right = True
+                elif event.key == ord('w'):
+                    self.ship.moving_up= True
+                elif event.key == ord('a'):
+                    self.ship.moving_left= True
+            elif event.type == pygame.KEYUP:
+                if event.key == ord('s'):
+                    self.ship.moving_down = False
+                elif event.key == ord('d'):
+                    self.ship.moving_right = False
+                elif event.key == ord('w'):
+                    self.ship.moving_up= False
+                elif event.key == ord('a'):
+                    self.ship.moving_left= False
 
 
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
 
 if __name__ == '__main__':
     ai= AlienInvasion()
