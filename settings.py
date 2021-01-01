584466c1f26f2393606cbea00bf88159e807d9a4
[ThtGuyBro] <[mrl20032000@gmail.com]>
2021-01-01 13:03:52 -0800

    Finnal coding session of 2020ish!

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ class Settings:
         self.screen_width=1200
         self.sceen_height=800
         self.bg_color = (230, 230, 230)
-        self.ship_speed = 3
+        self.ship_speed = .2
 
         self.bullet_speed = 5.0
         self.bullet_width = 3
